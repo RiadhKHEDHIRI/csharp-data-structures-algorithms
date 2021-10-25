90ad97751639db24d932ccd91b8b468353972c0e
Riadh Khedhiri <riadh.khedhiri@gmail.com>
2021-10-25 14:40:43 -0400

    Add Test project

---
+++ csharp-data-structures-algorithms-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharp_data_structures_algorithms_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
